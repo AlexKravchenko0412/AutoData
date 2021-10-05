28cc5bc18915471973a6709c9d7a89cf1fc0334e
AlexKravchenko0412 <alexkravchenko0412@gmail.com>
2021-10-05 23:43:09 +0300

    First activity layout added (without realisation) 23:42 Realization of the first layout added

--- app/src/main/java/android/example/autodata/MainActivity.java
+++ app/src/main/java/android/example/autodata/MainActivity.java
@@ -2,15 +2,37 @@ package android.example.autodata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextLogin;
+    private EditText editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextLogin = findViewById(R.id.editTextLogin);
+        editTextPassword = findViewById(R.id.editTextPassword);
+
+    }
+
+    public void onClickEnter(View view) {
+        String login = editTextLogin.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        if(!login.isEmpty() && !password.isEmpty()) {
+            Intent intent = new Intent(this, AutoDataWelcomeActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this,
+                    R.string.enter_all_fields,
+                    Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
