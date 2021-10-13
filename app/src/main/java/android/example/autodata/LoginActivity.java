4ef790f5abeded4f8a6a56db87d457a90155e638
AlexKravchenko0412 <alexkravchenko0412@gmail.com>
2021-10-13 16:10:08 +0300

    Working with Data Base

--- app/src/main/java/android/example/autodata/LoginActivity.java
+++ app/src/main/java/android/example/autodata/LoginActivity.java
@@ -2,16 +2,19 @@ package android.example.autodata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     private EditText editTextLogin;
     private EditText editTextPassword;
+    private UserDataDBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         editTextLogin = findViewById(R.id.editTextLogin);
         editTextPassword = findViewById(R.id.editTextPassword);
+        dbHelper = new UserDataDBHelper(this);
+
+
 
     }
 
@@ -33,6 +39,12 @@ public class MainActivity extends AppCompatActivity {
                     R.string.enter_all_fields,
                     Toast.LENGTH_SHORT).show();
         }
+
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(UserContract.UserData.COLUMN_LOGIN, login);
+        contentValues.put(UserContract.UserData.COLUMN_PASSWORD, password);
+        database.insert(UserContract.UserData.TABLE_NAME, null, contentValues);
 
     }
 }
